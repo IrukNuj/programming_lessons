81178a582a5cf4450f20a2840f1b37913ca30521
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2017-11-25 19:32:40 +0900

    injectで合計出したよ

---
+++ inject_sample.rb
@@ -0,0 +1,6 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+puts numbers.inject{|sum,n|sum + n}
+
+#　↑　injectの文章の書き方よくわからない。後で聞く
+# https://ref.xaio.jp/ruby/classes/enumerable/inject
+# http://blog.toshimaru.net/ruby-inject/

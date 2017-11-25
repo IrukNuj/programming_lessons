d5f844be0eede0178b66a2af90664664c9f4654a
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2017-11-25 19:45:49 +0900

    map!ｄｅ

---
+++ map_sample.rb
@@ -0,0 +1,14 @@
+puts "配列の個数を入力してください"
+
+a = gets.chomp.to_i
+m = []
+
+(0..a-1).each do |i|
+  puts "m[#{i}]を入力してください"
+  m[i] = gets.chomp.to_i
+end
+
+puts "倍にすると"
+p m.map!{|x|x*2}
+
+#　コイツもやっぱりわからないので聞こう...
